1ac69e27d21243bef07f147298ee121526fed639
Ethan Wong <ethanwongca@gmail.com>
2024-05-28 15:35:39 -0700

    Updating utils

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+import os 
+from dotenv import load_dotenv
+import google.generativeai as genai #Genai model 
 from utils.gemini import intialize_model
+
--- utils/gemini.py
+++ utils/gemini.py
@@ -2,13 +2,9 @@ import os
 from dotenv import load_dotenv
 import google.generativeai as genai #Genai model 
 
-def check_model():
-    for m in genai.list_models():
-        if 'generateContent' in m.supported_generation_methods:
-            print(m.name)
+#checks models available via Gemini API 
 
-
-#keeping the data modular
+#initialize the model
 def intialize_model():
     try:
         #loading the environment from the .env file 
@@ -20,13 +16,14 @@ def intialize_model():
             return None
         #intializing the model
         genai.configure(api_key=gemini_api_key)
-        model = genai.GenerativeModel('gemini-1.5-flash')
+        model = genai.GenerativeModel('gemini-pro')
         return model
     
     except Exception as e:
         print("Error in intializing the model")
         return None
 
+#prompts the model
 def prompt_model(model: str, prompt: str) -> str:
     try:
         response = model.generate_content(prompt)
