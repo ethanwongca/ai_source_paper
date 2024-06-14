54e159b5dbdd166598008e79a47a7611d2445a23
Ethan Wong <ethanwongca@gmail.com>
2024-06-13 18:55:18 -0700

    Removed llama

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import os 
-from dotenv import load_dotenv
-import google.generativeai as genai #Genai model 
-from utils.gemini import intialize_model
+import utils.gemini as gemini
+import utils.pdf_parser as parser
+from flask import Flask, request, jsonify, render_template, url_for, send_file
+import sqlite3
+import pandas as pd 
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+DATABASE = 'data/clinical_trials.db'
+EXEL_OUTPUT = 'data/clinical_trials.xlsx'
+
 
